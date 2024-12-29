3b2124aa0a49c3c1c0cd86d6e411c19a88bc56fb
Aldo Arista <aristaaldo2@gmail.com>
2024-12-30 03:31:08 +0800

    test husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Deploy now mfmfmfmffmfmfmfm
           </a>
           <a
             className="flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
